ff49491516abeb31cfe5e513e533d20d1bf17de2
Lilian Lee <lilianlee0213@gmail.com>
2023-08-24 14:18:18 -0400

    use auth.currentUser (property)

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import {useState} from 'react';
 import AppRouter from 'components/AppRouter';
+import {authService} from 'fbase';
 
 function App() {
-	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+	console.log(authService.currentUser); //null
 	return <AppRouter isLoggedIn={isLoggedIn} />;
 }
 
