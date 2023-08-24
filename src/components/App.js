5ee37c85aab7122d0f181302d85a8a9f3d6a677f
Lilian Lee <lilianlee0213@gmail.com>
2023-08-24 19:07:17 -0400

    check userAuth state and setLoggedIn true

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import AppRouter from 'components/AppRouter';
 import {authService} from 'fbase';
+import {onAuthStateChanged} from 'firebase/auth';
 
 function App() {
-	const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+	const [init, setInit] = useState(false);
+	const [isLoggedIn, setIsLoggedIn] = useState(false);
+	useEffect(() => {
+		//happens when first rendered
+		onAuthStateChanged(authService, (user) => {
+			//check if user is loggedIn
+			if (user) {
+				setIsLoggedIn(true);
+			} else {
+				setIsLoggedIn(false);
+			}
+			setInit(true);
+		});
+	}, []);
 	console.log(authService.currentUser); //null
-	return <AppRouter isLoggedIn={isLoggedIn} />;
+	return (
+		<>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initializing...'}</>
+	);
 }
 
 export default App;
