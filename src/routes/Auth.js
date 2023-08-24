bf2750be6f87ee816f44e3f3f53a0a6f36f49594
Lilian Lee <lilianlee0213@gmail.com>
2023-08-24 14:28:17 -0400

    add onChang and onSubmut function

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -3,15 +3,30 @@ import React, {useState} from 'react';
 const Auth = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const onChange = (event) => {
+		console.log(event.target.name);
+	};
+	const onSubmit = (event) => {
+		event.preventDefault();
+	};
 	return (
 		<div>
-			<form action="">
-				<input type="text" placeholder="Email" required value={email} />
+			<form onSubmit={onSubmit}>
+				<input
+					type="text"
+					name="email"
+					placeholder="Email"
+					required
+					value={email}
+					onChange={onChange}
+				/>
 				<input
 					type="password"
+					name="password"
 					placeholder="Passwrod"
 					required
 					value={password}
+					onChange={onChange}
 				/>
 				<input type="submit" value="Login" />
 			</form>
