1a8f2a6fdbe7c2f733b589f767809eaf4184f45a
Lilian Lee <lilianlee0213@gmail.com>
2023-08-24 14:26:07 -0400

    set up basic state and elements for login form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Auth = () => {
-	return <div>Auth</div>;
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	return (
+		<div>
+			<form action="">
+				<input type="text" placeholder="Email" required value={email} />
+				<input
+					type="password"
+					placeholder="Passwrod"
+					required
+					value={password}
+				/>
+				<input type="submit" value="Login" />
+			</form>
+			<div>
+				<button>Continue with Google</button>
+				<button>Continue with Github</button>
+			</div>
+		</div>
+	);
 };
 
 export default Auth;
