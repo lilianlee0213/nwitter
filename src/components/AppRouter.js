2c98e34cd707afa03344767ef0a7504cf564128a
Lilian Lee <lilianlee0213@gmail.com>
2023-08-24 22:16:05 -0400

    create navigation

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from '../routes/Home';
 import Auth from '../routes/Auth';
+import Navigation from './Navigation';
 const AppRouter = ({isLoggedIn}) => {
 	return (
 		<BrowserRouter>
+			{isLoggedIn && <Navigation />}
 			<Routes>
 				{isLoggedIn ? (
 					<Route exact path="/" element={<Home />} />
