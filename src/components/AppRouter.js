fe0c3a9fd8be36c286de1af2c010b2272dc25ae8
Lilian Lee <lilianlee0213@gmail.com>
2023-08-24 22:22:50 -0400

    logout function and redirect to home

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from '../routes/Home';
-import Auth from '../routes/Auth';
 import Navigation from './Navigation';
+import Home from '../routes/Home';
+import Profile from 'routes/Profile';
+import Auth from '../routes/Auth';
 const AppRouter = ({isLoggedIn}) => {
 	return (
 		<BrowserRouter>
 			{isLoggedIn && <Navigation />}
 			<Routes>
 				{isLoggedIn ? (
-					<Route exact path="/" element={<Home />} />
+					<>
+						<Route exact path="/" element={<Home />} />
+						<Route exact path="/profile" element={<Profile />} />
+					</>
 				) : (
 					<Route exact path="/" element={<Auth />} />
 				)}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,18 @@
+import {authService} from 'fbase';
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 
 const Profile = () => {
-	return <div>Profile</div>;
+	const redirectHome = useNavigate();
+	const onLogOutClick = () => {
+		authService.signOut();
+		redirectHome('/');
+	};
+	return (
+		<>
+			<button onClick={onLogOutClick}>Log Out</button>
+		</>
+	);
 };
 
 export default Profile;
