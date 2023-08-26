f2d43dc7a29b84e6c1a11c50bde3bbd28c395b6d
Lilian Lee <lilianlee0213@gmail.com>
2023-08-26 19:11:08 -0400

    Initialize Storage, get a reference to the service

--- src/fbase.js
+++ src/fbase.js
@@ -1,6 +1,7 @@
 import {initializeApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
+import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_API_KEY,
@@ -15,3 +16,4 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 export const authService = getAuth(firebaseApp);
 export const dbService = getFirestore(firebaseApp);
+export const storageService = getStorage(firebaseApp);
