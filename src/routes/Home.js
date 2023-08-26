c7b8f3cfad1e94fb8be730a83c3e03907c848548
Lilian Lee <lilianlee0213@gmail.com>
2023-08-25 20:10:31 -0400

    get nweets (data doc)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,17 @@
 import {dbService} from 'fbase';
-import {addDoc, collection} from 'firebase/firestore';
-import React, {useState} from 'react';
+import {addDoc, collection, getDocs} from 'firebase/firestore';
+import React, {useEffect, useState} from 'react';
 
 const Home = () => {
 	const [nweet, setNweet] = useState('');
+	const [nweets, setNweets] = useState([]);
+	const getNweets = async () => {
+		const dbNweets = await getDocs(collection(dbService, 'nweets'));
+		dbNweets.forEach((doc) => setNweets((prev) => [doc.data(), ...prev]));
+	};
+	useEffect(() => {
+		getNweets();
+	}, []);
 	const onSubmit = async (event) => {
 		event.preventDefault();
 		try {
