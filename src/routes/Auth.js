7505930aaa1d38d948adbc13f9c03f0ef84dae13
Lilian Lee <lilianlee0213@gmail.com>
2023-08-30 15:13:34 -0400

    auth styles added

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -6,6 +6,12 @@ import {
 	signInWithPopup,
 } from 'firebase/auth';
 import AuthForm from 'components/AuthForm';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {
+	faGithub,
+	faGoogle,
+	faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Auth = () => {
 	const [error, setError] = useState('');
@@ -28,14 +34,22 @@ const Auth = () => {
 		}
 	};
 	return (
-		<div>
+		<div className="authContainer">
+			<FontAwesomeIcon
+				icon={faTwitter}
+				color={'#05aaff'}
+				size="3x"
+				style={{marginBottom: 30}}
+			/>
 			<AuthForm error={error} setError={setError} />
-			<div>
-				<button name="google" onClick={onSocialClick}>
+			<div className="authBtns">
+				<button name="google" onClick={onSocialClick} className="authBtn">
 					Continue with Google
+					<FontAwesomeIcon icon={faGoogle} />
 				</button>
-				<button name="github" onClick={onSocialClick}>
+				<button name="github" onClick={onSocialClick} className="authBtn">
 					Continue with Github
+					<FontAwesomeIcon icon={faGithub} />
 				</button>
 			</div>
 		</div>
