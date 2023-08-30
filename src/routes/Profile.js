07578828e3e1533b9b48298c1416f90d10d4db8e
Lilian Lee <lilianlee0213@gmail.com>
2023-08-29 20:01:03 -0400

    create query

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,25 @@
-import {authService} from 'fbase';
-import React from 'react';
+import {authService, dbService} from 'fbase';
+import {collection, orderBy, query, where} from 'firebase/firestore';
+import React, {useEffect} from 'react';
 import {useNavigate} from 'react-router-dom';
 
-const Profile = () => {
+const Profile = ({userObj}) => {
 	const redirectHome = useNavigate();
 	const onLogOutClick = () => {
 		authService.signOut();
 		redirectHome('/');
 	};
+	const getMyNweets = async () => {
+		const nweetsRef = collection(dbService, 'nweets');
+		const q = query(
+			nweetsRef,
+			where('creatorId', '==', userObj.uid),
+			orderBy('createdAt', 'desc')
+		);
+	};
+	useEffect(() => {
+		getMyNweets();
+	}, []);
 	return (
 		<>
 			<button onClick={onLogOutClick}>Log Out</button>
