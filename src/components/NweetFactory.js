a33db300d00c0c6901c154ff23f97952febe4971
Lilian Lee <lilianlee0213@gmail.com>
2023-08-30 15:26:41 -0400

    nweet factory styles

--- src/components/NweetFactory.js
+++ src/components/NweetFactory.js
@@ -3,6 +3,8 @@ import {dbService, storageService} from 'fbase';
 import {addDoc, collection} from 'firebase/firestore';
 import {ref, uploadString, getDownloadURL} from 'firebase/storage';
 import {v4 as uuidv4} from 'uuid';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faPlus, faTimes} from '@fortawesome/free-solid-svg-icons';
 
 const NweetFactory = ({userObj}) => {
 	const [nweet, setNweet] = useState('');
@@ -49,20 +51,43 @@ const NweetFactory = ({userObj}) => {
 		setAttachment(null);
 	};
 	return (
-		<form onSubmit={onSubmit}>
+		<form onSubmit={onSubmit} className="factoryForm">
+			<div className="factoryInput__container">
+				<input
+					className="factoryInput__input"
+					value={nweet}
+					onChange={onChange}
+					type="text"
+					placeholder="What's on your mind?"
+					maxLength={120}
+				/>
+				<input type="submit" value="&rarr;" className="factoryInput__arrow" />
+			</div>
+			<label htmlFor="attach-file" className="factoryInput__label">
+				<span>Add photos</span>
+				<FontAwesomeIcon icon={faPlus} />
+			</label>
 			<input
-				type="text"
-				placeholder="What's on your mind?"
-				maxLength={120}
-				onChange={onChange}
-				value={nweet}
+				id="attach-file"
+				type="file"
+				accept="image/*"
+				onChange={onFileChange}
+				style={{
+					opacity: 0,
+				}}
 			/>
-			<input type="file" accept="image/*" onChange={onFileChange} />
-			<input type="submit" value="Nweet" />
 			{attachment && (
-				<div>
-					<img src={attachment} width={50} height={50} alt="attachment" />
-					<button onClick={onClearAttachmentClick}>Clear Photo</button>
+				<div className="factoryForm__attachment">
+					<img
+						src={attachment}
+						style={{
+							backgroundImage: attachment,
+						}}
+					/>
+					<div className="factoryForm__clear" onClick={onClearAttachmentClick}>
+						<span>Remove</span>
+						<FontAwesomeIcon icon={faTimes} />
+					</div>
 				</div>
 			)}
 		</form>
