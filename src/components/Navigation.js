ca993f208727efc171a6dc46c852ba5908879c8b
Lilian Lee <lilianlee0213@gmail.com>
2023-08-30 15:20:13 -0400

    navigation styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,23 @@
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
+import {faUser} from '@fortawesome/free-regular-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import React from 'react';
 import {Link} from 'react-router-dom';
 
 const Navigation = ({userObj}) => {
 	return (
 		<nav>
-			<ul>
+			<ul className="nav-links">
 				<li>
-					<Link to="/">Home</Link>
+					<Link to="/" className="nav-link-logo">
+						<FontAwesomeIcon icon={faTwitter} color={'#04aaff'} size="2x" />
+					</Link>
 				</li>
 				<li>
-					<Link to="/profile">{userObj.displayName}'s Profile</Link>
+					<Link to="/profile" className="nav-link-profile">
+						<FontAwesomeIcon icon={faUser} color={'#04AAFF'} size="2x" />
+						<span> {userObj.displayName}'s Profile</span>
+					</Link>
 				</li>
 			</ul>
 		</nav>
