b44be87c9043cd61f94d1729094384ae56ee3bfc
Lilian Lee <lilianlee0213@gmail.com>
2023-08-30 19:16:37 -0400

    brower router basename

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -6,7 +6,7 @@ import Profile from 'routes/Profile';
 import Auth from '../routes/Auth';
 const AppRouter = ({isLoggedIn, userObj, refreshUser}) => {
 	return (
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			{isLoggedIn && <Navigation userObj={userObj} />}
 			<Routes>
 				{isLoggedIn ? (
